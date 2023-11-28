6c50d2dadb9f42f9623557498c669461f923656b
Matt Johnson <matthew_johnson@loomis.org>
2023-11-28 10:47:47 -0500

    11/28

---
+++ unit_1/hello_universe.py
@@ -0,0 +1,11 @@
+import random
+message = 'Hello, World'
+print(message)
+
+message = int(input('Enter a number: '))
+
+print(message, 'Hello, World')
+print(f'Hello, class. {random.randint(5, 100) * message} happy new term!')
+
+
+
