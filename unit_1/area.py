16c042a018a022e628cfc839ef21f6d170943f3f
Matt Johnson <matthew_johnson@loomis.org>
2023-11-28 11:42:52 -0500

    11/28

---
+++ unit_1/area.py
@@ -0,0 +1,6 @@
+length = int(input('Enter a length: '))
+width = int(input('Eneter a width: '))
+
+area = length * width
+
+print(f'The area of the rectangle you entered is: {area}. Thanks for feeding me yummy data.')
