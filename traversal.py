900b18927d10d8eda005d841fac88e5c1f8e7969
Matt Johnson <matthew_johnson@loomis.org>
2023-12-06 20:10:58 -0500

    12/6

---
+++ traversal.py
@@ -0,0 +1,5 @@
+string_parse = [1, 5, 9, 18, 10, 11, 36, 0, 13, 2, 1, 4, 5, 15]
+string_parse = '''Four score and seven years ago, 
+                our fathers brought forth to this land a new nation, 
+                dedicated to the proposition that all men are created equal'''
+
