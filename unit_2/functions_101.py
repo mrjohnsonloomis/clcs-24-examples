da6a827c2136236575a7de68cc9ccc84fe0f2110
Matt Johnson <matthew_johnson@loomis.org>
2024-01-16 12:25:27 -0500

    1/16

---
+++ unit_2/functions_101.py
@@ -0,0 +1,12 @@
+# function definition at the top of the program 
+def add(x, y):
+    print(f"Adding {x} and {y}...")
+    return (x + y)
+
+def sad():
+    print ('The Eagles killed any sense of happiness I had.')
+    
+# main code here
+sum = add(24832904, 128943)
+sum = sum + add(12, 4)
+print(f"The sum is: {sum}")
