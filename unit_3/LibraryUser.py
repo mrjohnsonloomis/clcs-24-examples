88459f4e1b361a1fa676da482964f6286346cf72
Matt Johnson <matthew_johnson@loomis.org>
2024-02-16 11:07:42 -0500

    2/16

---
+++ unit_3/LibraryUser.py
@@ -0,0 +1,53 @@
+class LibraryUser:
+
+    id_counter = 1
+
+#Constructor -- takes in a list of info as a parameter
+    def __init__(self, info):
+        '''info is a list read in from a line from a form on the website (which is stored in a csv file)'''
+        self.username = info[0]
+        self.password = info[1]
+        self.first_name = info[2]
+        self.last_name = info[3]
+        self.address = info[4]
+        self.phone = info[5]
+        self.is_active = True
+        self.id_num = LibraryUser.id_counter
+        self.has_late = False
+        self.books_out = []
+        self.book_history = []
+
+        #increases the ID coutner to create a new, unique ID each time the constructor is called
+        LibraryUser.id_counter += 1
+
+
+#Instance Methods
+
+    def check_out(self, book):
+        '''checks out a book @param is a book object'''
+        if book.is_out == False:
+            book.is_out = True
+
+            #updates the list of books out as well as user history
+            self.books_out.append(book)
+            self.book_history.append(book)
+        
+    
+    def is_late(self):
+        '''sets whether a user has a late item and owes a fee'''
+        for books in self.books_out:
+            if books.is_late: self.has_late = True
+
+    def return_book(self, book):
+        '''returns a book'''
+        if book in self.books_out:
+            self.books_out.remove(book)
+        else:
+            print('Error: Patron does not have that book checked out.')
+
+    def get_book_history(self):
+        '''returns a list of book titles user has taken out'''
+        str_books = []
+        for book in self.book_history:
+            str_books.append(book.get_title())
+        return str_books
---
+++ unit_3/library_test.py
@@ -0,0 +1,12 @@
+from LibraryUser import *
+from Book import *
+
+info = ['mjohnson', 'password', 'Matt', 'Johnson', '4 Batchelder Road, Windsor CT', '6094397160']
+user1 = LibraryUser(info)
+
+book1 = Book('On Beauty','Zadie Smith')
+
+user1.check_out(book1)
+print(book1.is_out)
+
+print(user1.get_book_history())
