6c8e37a3626f2c82e350f124e80152d6e9eab0e8
Matt Johnson <matthew_johnson@loomis.org>
2024-03-24 11:53:55 -0400

    3/24

--- unit_3/access_modifiers.py
+++ unit_3/access_modifiers.py
@@ -3,7 +3,8 @@ from Account import Account
 account = Account("John Doe", 1000)
 print(account._owner)  # This will print "John Doe"
 #print(account.__balance)  # This will raise an AttributeError
-
+print(account._Account__balance)  # This will print 1000
+#print(dir(account))  # This will show the __balance attribute
 account.deposit(500)
 account.withdraw(200)
 print(f"Balance: {account.get_balance()}")
