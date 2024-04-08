2b85e68ca8c65da1f1184f9f8d0295a5ad18ff66
Matt Johnson <matthew_johnson@loomis.org>
2024-04-08 10:18:00 -0400

    4/8 edit

--- unit_4/nba_stats.py
+++ unit_4/nba_stats.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://api.balldontlie.io/v1/players/?search={name}'
-api_key = #your key here
+#api_key = #your key here
 header = {'Authorization': api_key}
 
 r = requests.get(url, headers=header)
